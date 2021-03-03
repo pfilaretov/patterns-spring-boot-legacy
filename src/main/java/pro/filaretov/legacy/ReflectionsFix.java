c868f2c3edab310d911b59345288c33d0c7f82e5
Petr Filaretov <petr.filaretov@gmail.com>
2021-03-03 14:54:31 +0300

    integrate other framework into spring boot app - workaround reflections library issue

---
+++ src/main/java/pro/filaretov/legacy/ReflectionsFix.java
@@ -0,0 +1,10 @@
+package pro.filaretov.legacy;
+
+/**
+ * This useless enum is here just to fix an issue with reflections library
+ * which is used in patterns-spring-boot-starter project to scan this project's classes.
+ * See here for details: https://github.com/ronmamo/reflections/issues/273
+ */
+public enum ReflectionsFix {
+    FIX
+}
